906fe8cf9d169e4e965b2dac38addaddbfd2cdec
wellesleyhacks <33560629+pegolegoeggo@users.noreply.github.com>
2020-08-08 17:03:40 -0700

    initial profile design

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 import os
 import dbi
 import imghdr
-import bcrypt
+#import bcrypt
 #import db # database stuff
 import json
 
@@ -20,14 +20,22 @@ def index():
     return render_template('home.html', page_title='Dormir')
 
 
+@app.route('/profile/')
+def profile():
+    # if "username" in session:
+    #    return redirect(url_for("profile"))
+    return render_template('profile.html', page_title='Dormir')   
+
+
 
 if __name__ == '__main__':
-    import sys,os
-    if len(sys.argv) > 1:
-        # arg, if any, is the desired port number
-        port = int(sys.argv[1])
-        assert(port>1024)
-    else:
-        port = os.getuid()
-    app.debug = True
-    app.run('0.0.0.0',port)
+    # import sys,os
+    # if len(sys.argv) > 1:
+    #     # arg, if any, is the desired port number
+    #     port = int(sys.argv[1])
+    #     assert(port>1024)
+    # else:
+    #     port = os.getuid()
+    # app.debug = True
+    #app.run('0.0.0.0',port)
+    app.run()
